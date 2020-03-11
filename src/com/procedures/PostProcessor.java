179f63c7220e9cdeecc896fa726d2c3e62d0e62f
Andrew <andrewburton345@gmail.com>
2020-03-11 14:50:45 -0600

    Argument Parser Clean Up

--- src/com/procedures/PostProcessor.java
+++ src/com/procedures/PostProcessor.java
@@ -1,4 +1,13 @@
 package com.procedures;
 
+/**
+ * Post-Processor is a class designed to handle any clean up and or knowledge validation post learning phase.
+ *
+ * @author Andrew Burton
+ * @since %i%, %G%
+ * @see com.procedures.Genetic
+ * @see com.procedures.PreProcessor
+ */
 public class PostProcessor {
+    // TODO
 }
