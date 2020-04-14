d98a02e52d3459b19fc998ade0cf02a5130bdaaf
Andrew <andrewburton345@gmail.com>
2020-04-14 13:06:02 -0600

    maybe fixed remove duplicate project

--- src/main/java/ca/ucalgary/rules599/Training/src/main/java/ca/ucalgary/rules599/model/Item.java
+++
@@ -1,8 +0,0 @@
-package ca.ucalgary.rules599.model;
-
-import java.io.Serializable;
-
-
-public interface Item extends Comparable<Item>, Serializable {
-
-}
--- src/test/java/ca/ucalgary/rules599/test/PreProcessorTest.java
+++ src/test/java/ca/ucalgary/rules599/test/PreProcessorTest.java
@@ -1,6 +1,5 @@
 package ca.ucalgary.rules599.test;
 
-import ca.ucalgary.rules599.Training.GAImplementation;
 import ca.ucalgary.rules599.Training.Preprocessor;
 import ca.ucalgary.rules599.Training.Processor;
 import ca.ucalgary.rules599.datastructure.TransactionalItemSet;
@@ -24,7 +23,6 @@ public class PreProcessorTest {
 
     private Preprocessor preprocessor;
     private Processor processor;
-    private GAImplementation gaImplementation;
 
     @Before
     public void setup() {
@@ -41,7 +39,7 @@ public class PreProcessorTest {
     @Test
     public final void InitialLoadTest() {
         File file = new File(INPUT_FILE_1);
-        List<AccidentData> actualData = preprocessor.processInitialData(file,"src/test/resources/output.txt", 0.3f);
+        List<AccidentData> actualData = preprocessor.processInitialData(file,"src/test/resources/output.txt");
         assertEquals(actualData.size(), 749);
     }
 
@@ -51,7 +49,6 @@ public class PreProcessorTest {
         assertNotNull(actualData);
     }
 
-
     @Test
     public final void InitialProcessingGenerateFrequentItemsTest() {
         Map<Integer, TransactionalItemSet<AccidentAttribute>> actualData = processor.findFrequentItemSets("src/test/resources/driversTest.csv",0.8);
@@ -59,72 +56,10 @@ public class PreProcessorTest {
     }
 
     @Test
-    public final void generateSpecificFrequentItemsTest() {
-        Map<Integer, TransactionalItemSet<AccidentAttribute>> actualData = processor.findnItemFrequentItemSets("src/test/resources/driversTest.csv",0.8,1, 5);
-        assertNotNull(actualData);
-    }
-
-
-
-    @Test
     public final void InitialProcessingGenerateGeneticFrequentItemsTest() {
         List<Map<String, int[]>> actualData = processor.findGeneticFrequentItemSets("src/test/resources/driversTest.csv",0.8,"src/test/resources/accidentAttributeValues.yml");
         assertNotNull(actualData);
     }
-
-
-
-    @Test
-    public final void GATestLoadingExitingRules() {
-        Population population =  new Population();
-        gaImplementation = new GAImplementation(population);
-        Map<Integer,TransactionalItemSet<AccidentAttribute>> actualData=gaImplementation.updatedFrequentItemSets("");
-        assertNotNull(actualData);
-    }
-
-
-    @Test
-    public final void GACrossOverAndMutationTest() {
-        Population population =  new Population();
-        population.initializePopulation(processor.findnItemFrequentItemSets("src/test/resources/driversTest.csv",0.8,1, 5),5);
-        gaImplementation = new GAImplementation(population);
-        Population actualData =gaImplementation.runGA();
-        assertNotNull(actualData);
-    }
-
-
-    @Test
-    public final void GAOutputTest() {
-        Apriori.Configuration configuration = new Apriori.Configuration();
-        double minSupport = 0.5;
-        double maxSupport = 0.8;
-        double supportDelta = 0.2;
-        int frequentItemSetCount = 500;
-        boolean generateRules = true;
-        double minConfidence = 0.8;
-        double maxConfidence = 0.9;
-        double confidenceDelta = 0.2;
-        int ruleCount = 400;
-        configuration.setMinSupport(minSupport);
-        configuration.setMaxSupport(maxSupport);
-        configuration.setSupportDelta(supportDelta);
-        configuration.setFrequentItemSetCount(frequentItemSetCount);
-        configuration.setGenerateRules(generateRules);
-        configuration.setMinConfidence(minConfidence);
-        configuration.setMaxConfidence(maxConfidence);
-        configuration.setConfidenceDelta(confidenceDelta);
-        configuration.setRuleCount(ruleCount);
-
-
-        Population population =  new Population();
-        population.initializePopulation(processor.findnItemFrequentItemSets("src/test/resources/driversTest.csv",0.8,1, 5),5);
-        gaImplementation = new GAImplementation(population);
-        Output<AccidentAttribute> actualData =gaImplementation.outputSortedRules("src/test/resources/driversTest.csv", "src/test/resources/knownRulest.txt", configuration);
-        assertNotNull(actualData);
-    }
-
-
-
 
     /**
     *
